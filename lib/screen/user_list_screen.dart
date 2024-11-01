651ab3f17c641662a3f2e5e7723faee503b8680d
Darus Salam <darussalam66d@gmail.com>
2024-11-01 08:57:28 +0600

    update

--- lib/screen/user_list_screen.dart
+++ lib/screen/user_list_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:myfirstapp/screen/user_list_screen.dart';
 
-class ProductScreen extends StatelessWidget {
-  const ProductScreen({super.key});
+class UserListScreen extends StatelessWidget {
+  const UserListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product_Screen"),
+        title: Text("UserListScreen"),
       ),
       body: Column(
         children: [
@@ -16,14 +15,15 @@ class ProductScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Center(child: ElevatedButton(onPressed: (){
               Navigator.pop(context);
-            }, child: Text("back to privous page"),),
-            ),
+            }, child: Text("back to privous page"),),),
           ),
-
-
-
           Center(child: ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>UserListScreen()));
+
+
+            // Navigator.push(context, MaterialPageRoute(builder: (context)=>UserListScreen()));
+
+
+
           }, child: Text("Next"),),),
         ],
       ),
